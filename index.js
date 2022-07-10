701a73114b3237625bc487e852ca2e60d3e7fd08
Bernhard Kau <bernhard.kau@vcat.de>
2022-07-10 18:03:34 +0200

    some coding style fixes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 wp.blocks.registerBlockType(
-    'rpssrb/random-posts',
-    {
-        title: 'Random Posts',
-        edit: function () {
-            return wp.element.createElement(
-                wp.serverSideRender, {
-                    block: 'rpssrb/random-posts'
-                }
-            );
-        }
-    }
+	'rpssrb/random-posts',
+	{
+		title: 'Random Posts',
+		edit: function () {
+			return wp.element.createElement(
+				wp.serverSideRender,
+				{
+					block: 'rpssrb/random-posts'
+				}
+			);
+		}
+	}
 );
